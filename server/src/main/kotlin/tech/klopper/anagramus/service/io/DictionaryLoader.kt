e0fea753d159dcbd664cabb2186ac30d03187147
Natie <natieklopper@gmail.com>
2019-06-18 12:08:56 +1000

    dictionary controller and loader

--- server/src/main/kotlin/tech/klopper/anagramus/service/io/DictionaryLoader.kt
+++ server/src/main/kotlin/tech/klopper/anagramus/service/io/DictionaryLoader.kt
@@ -1,20 +1,26 @@
 package tech.klopper.anagramus.service.io
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Service
 import tech.klopper.anagramus.model.WordModel
-import java.io.FileInputStream
+import java.io.File
 import java.io.FileNotFoundException
-import java.nio.charset.Charset
 
 @Service
-class DictionaryLoader : Dictionary {
+@Configuration
+class DictionaryLoader(
+        @Value("\${tech.klopper.anagramus.dictionary.path}") private val dictPath: String
+) : Dictionary {
 
     private var wordList: MutableList<WordModel> = mutableListOf()
 
     init {
         try {
-            val inputStream = FileInputStream("words_alpha.txt")
-            readIntoWords(inputStream)
+            val wordListTmp = File(dictPath).useLines { it.toList() }
+            for (word in wordListTmp) {
+                wordList.add(WordModel(word))
+            }
         } catch (e: FileNotFoundException) {
             e.printStackTrace()
         }
@@ -22,14 +28,6 @@ class DictionaryLoader : Dictionary {
 
     override fun getWordList(): List<WordModel> {
         return wordList
-    }
-
-    private fun readIntoWords(inputStream: FileInputStream) {
-        val content = inputStream.readBytes().toString(Charset.defaultCharset())
-        val wordListTmp = content.split('\n') as ArrayList<String>
-        for (word in wordListTmp){
-            wordList.add(WordModel(word))
-        }
     }
 }
 
