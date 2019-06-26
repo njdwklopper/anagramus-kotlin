4b2971fb21ab05a86c622bc65cf01c651a349731
Natie <natieklopper@gmail.com>
2019-06-26 11:22:52 +1000

    still wip on xamarin firebase setup, ios is still a fickle one thus far

--- client/xamarin/Anagramus/Anagramus.iOS/service/_base/session/FirebaseIOSUserHandler.cs
+++ client/xamarin/Anagramus/Anagramus.iOS/service/_base/session/FirebaseIOSUserHandler.cs
@@ -1,6 +1,14 @@
+using Foundation;
+using Google.SignIn;
+using UIKit;
+
 namespace Anagramus.iOS.service._base.session
 {
-    public class FirebaseIOSUserHandler
+    public class FirebaseIOSUserHandler :  UIViewController, ISignInDelegate, ISignInUIDelegate
     {
+        public void DidSignIn(SignIn signIn, GoogleUser user, NSError error)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
